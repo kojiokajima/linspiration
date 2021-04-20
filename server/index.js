7d86bc561facfa90a52ca79882560eb869091629
wanko <kouji124@w4.dion.ne.jp>
2021-04-19 18:17:29 -0700

    uncommented cors part

--- server/index.js
+++ server/index.js
@@ -16,11 +16,11 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, '../client/build')))
-// app.use(cors({
-//   origin: ["http://localhost:3000"],
-//   methods: ["GET", "POST"],
-//   credentials: true,
-// }))
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  methods: ["GET", "POST"],
+  credentials: true,
+}))
 app.use(session({
   key: "token",
   secret: process.env.NODE_SESSION_SECRET,
