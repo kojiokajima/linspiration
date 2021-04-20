5bb53d9fe1ba18e0df3b4654b294437aa75f57fc
wanko <kouji124@w4.dion.ne.jp>
2021-04-19 18:43:13 -0700

    chenged route

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import { SignUp, SignIn, Dashboard } from "./components/index";
+import { SignUp, SignIn, Dashboard, Home } from "./components/index";
 
 const Router = () => {
   return (
     <Switch>
-      <Route exact path="/" component={SignIn} />
+      <Route exact path="/" component={Home} />
       <Route exact path="/signup" component={SignUp} />
       <Route path="/signin" component={SignIn} />
       <Route path="/dashboard/:id" component={Dashboard} />
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import {Link} from  'react-router-dom'
+import {Button} from 'react-materialize'
 
 const Home = () => {
   return (
     <div>
-      This is Home
+      <h1>This is home</h1>
+      <br/><br/>
+      <Link to="/signin">
+        <Button type="submit" waves="light">Login</Button>
+      </Link>
+      <br/><br/>
+      <Link to="/signup">
+        <Button type="submit" waves="light">Signup</Button>
+      </Link>
+
     </div>
   )
 }
